a908eaf713a5d65d2c44093cea1d42ba9c16e008
f1uff3h <peco@camarad.tech>
2024-09-06 19:17:39 +0300

    20240906

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 LazyVim.terminal.setup("nu")
+
+local opt = vim.opt
+
+opt.spell = true
+opt.spelllang = "en_us"
+opt.wrap = true
