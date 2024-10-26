01a810e8b055e34122adfb38c98bf01c80e2dab8
f1uff3h <peco@camarad.tech>
2024-10-26 18:31:44 +0300

    20241026

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,10 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-map("n", "gct", "O<esc>VcTODO: <esc><cmd>normal gcc<cr>A", { desc = "Add TODO comment above current line" })
--- HACK: is there a better way to do this?
-map("n", "gcat", "<cmd>normal gct<cr><esc>0v$d$a <esc>p$jddA", { desc = "Add TODO comment at the end of line" })
+map("n", "gcT", "O<esc>VcTODO: <esc><cmd>normal gcc<cr>A", { desc = "Add TODO comment above current line" })
+map("n", "gct", "o<esc>VcTODO: <esc><cmd>normal gcc<cr>A", { desc = "Add TODO comment below current line" })
+map("n", "gcN", "O<esc>VcNOTE: <esc><cmd>normal gcc<cr>A", { desc = "Add NOTE comment above current line" })
+map("n", "gcn", "o<esc>VcNOTE: <esc><cmd>normal gcc<cr>A", { desc = "Add NOTE comment below current line" })
+map("n", "gcF", "O<esc>VcFIXME: <esc><cmd>normal gcc<cr>A", { desc = "Add FIXME comment above current line" })
+map("n", "gcf", "O<esc>VcFIXME: <esc><cmd>normal gcc<cr>A", { desc = "Add FIXME comment below current line" })
 map("n", "<leader>n", "<cmd>Neotree focus<cr>", { desc = "focus Neotree" })
